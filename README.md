6284ab8171f114b6a3dc9eb1369a2bac77f611a0
albertlast <mathiaspapealbert@hotmail.com>
2023-01-22 11:27:41 +0100

    convert vue project to svelte

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# GameLiveCounterSvelte
- Game Liv Counter in Svelte
+# GameLiveCounter with Svelte
+
+Is a basic live counter for games,
+it can handle two person at the same time.
+
+You can use the setting menu to switch to fullscreen,
+also when android with chromebase browser is used,
+you can also disable the screensaver (enable wakelock).
+
+![App](doc/App.png)
+
+## Requirments
+
+Only a webserver to deliver the js files is needed,
+the website runs full on client side.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Run Unit Tests with [Vitest](https://vitest.dev/)
+
+```sh
+npm run test:unit
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import PlayerView from './components/PlayerView.svelte'
+  import SettingView from './components/SettingView.svelte'
+  import ReSettings5Line from './icons/ReSettings5Line.svelte'
+
+  let showSettings: boolean = false
+</script>
+
+<main class="w-screen h-screen">
+  <div
+    id="openSettings"
+    class="openSetting"
+    on:click={() => (showSettings = !showSettings)}
+    on:keypress={() => (showSettings = !showSettings)}
+  >
+    <ReSettings5Line />
+  </div>
+  <SettingView {showSettings} on:click={() => (showSettings = !showSettings)} />
+  <PlayerView playerName="first" class="PlayerView rotate-180 bg-cyan-900" style="height: 50vh" />
+  <PlayerView playerName="second" class="PlayerView bg-yellow-600" style="height: 50vh" />
+</main>
+
+<style>
+  main {
+    color: rgba(235, 235, 235, 0.64);
+    user-select: none;
+    -webkit-user-select: none;
+  }
+  /* :global(.visible) {
+    opacity: 1;
+  }
+  :global(.invisible) {
+    opacity: 0;
+  } */
+
+  :global(.PlayerView) {
+    font-size: min(28vh, 30vw);
+  }
+  .openSetting {
+    position: absolute;
+    top: 50%;
+    right: 50%;
+    transform: translate(50%, -50%);
+    z-index: 5;
+    font-size: min(14vh, 14vw);
+  }
+</style>
